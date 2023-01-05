8135e44f513c01df8cba525720fa0f9e276b2f70
sergioruydev <sergioruyenator@gmail.com>
2023-01-05 18:17:01 -0300

    Teste hello World.

--- department-service/src/main/java/com/sergioruy/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/sergioruy/departmentservice/DepartmentServiceApplication.java
@@ -8,6 +8,7 @@ public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DepartmentServiceApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
