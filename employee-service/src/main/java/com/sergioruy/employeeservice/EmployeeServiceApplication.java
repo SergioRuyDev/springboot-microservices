9ffb0db04283deb3a75a810486902c650978bf0d
sergioruydev <sergioruyenator@gmail.com>
2023-01-10 11:03:51 -0300

    Eureka Client Server Registry implemented.

--- employee-service/src/main/java/com/sergioruy/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/sergioruy/employeeservice/EmployeeServiceApplication.java
@@ -2,10 +2,12 @@ package com.sergioruy.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients //Find Components with Feigh Clients Annotations
+@EnableEurekaClient //In springboot 3 don't need this annotation
 public class EmployeeServiceApplication {
 
 //	@Bean
