2ae298489299a6e3d53fe12341b97820091064b0
sergioruydev <sergioruyenator@gmail.com>
2023-01-16 16:05:25 -0300

    New microservice on progress.

---
+++ organization-services/src/main/java/com/sergioruy/organizationservices/OrganizationServicesApplication.java
@@ -0,0 +1,13 @@
+package com.sergioruy.organizationservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizationServicesApplication.class, args);
+	}
+
+}
---
+++ organization-services/src/test/java/com/sergioruy/organizationservices/OrganizationServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergioruy.organizationservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
